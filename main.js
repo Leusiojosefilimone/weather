1d427cb0b538dc8d58090a43acae57df06dba55f
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-01-03 11:09:55 -0300

    dinamizando data e opções

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
+const cityContainer = document.querySelector('#citys');
+const country = ["Maputo", "Xai-xai", "Inhambane", "Chimoio", "Beira", "Tete", "Quelimane", "Nampula", "Pemba", "Lichinga" ];
+const loading = document.querySelector('.loading');
+
+country.map((city) => {
+    cityContainer.innerHTML += ` <option value=${city}>${city}</option> `
+})
 
 let weather = {
     apiKey: 'c4b9325197af37f29efbe8e401ab6039',
-    fetchWeather: function (city) {
-      fetch(
-        //    " https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=c4b9325197af37f29efbe8e401ab6039"
-        'https://api.openweathermap.org/data/2.5/weather?q=' +
-          city +
-          '&units=imperial&appid=' +
-          this.apiKey
-      )
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data))
+    fetchWeather: async function (city) {
+      loading.style.display = 'block';
+      const Promisededados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city +'&units=imperial&appid=' +this.apiKey)
+      const json =  await  Promisededados.json();
+      setTimeout(() => {
+        loading.style.display = 'none';
+      }, 2000);
+        return this.displayWeather(json)
     },
   
-    displayWeather: function (data) {
-      const { name } = data
-      const { description, icon } = data.weather[0]
-      const { temp, humidity } = data.main
+    displayWeather: function (json) {
+      const { name } = json
+      const { description, icon } = json.weather[0]
+      const { temp, humidity } = json.main
   
       console.log(name, icon, description, temp, humidity)
 
-      document.querySelector('.temperatura').innerText = Math.floor(temp) + '°F'
+      document.querySelector('.temperatura').innerText = Math.floor((temp - 32 ) / 1.8) + '°C'
 
       document.querySelector('label').innerText =
         'Humidity: ' + humidity + '%'
@@ -29,28 +34,19 @@ let weather = {
       'https://openweathermap.org/img/wn/' + icon + '.png'
 
       const imagen =document.querySelector('.icon-ceu')
-      imagen.style.background = "url('https://source.unsplash.com/550x350/?" + name + "')"
-     
-
-      document.querySelector('.weather').classList.remove('loading')
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')"
-
-        
-        function gerarMes (){
-        const datas = new Date;
-        const dia  = datas.getDay()
-            const meses = [jan, fev, mar, april, may, jun, jul, ago, sep, opt, nov, dez] 
-            const mes = meses[datas.getMonth()]
-            document.querySelector('.data').innerText = "mes"
-        }
-
-      
+      imagen.style.background = "url('https://source.unsplash.com/750x500/?" + name + "')"
     },
+
     search: function () {
       this.fetchWeather(document.querySelector('#citys').value)
     },
   }
 
+ const datas = new Date;
+        const dia  = datas.getDate()
+        const meses = ['jan', 'fev', 'mar', 'april', 'may', 'jun', 'jul', 'ago', 'sep', 'nov', 'dez'] 
+        let mes = meses[datas.getMonth()]
+        document.querySelector('#data').innerText = dia +" "+ mes
+      console.log(dia)
   // weather.displayWeather('London')
   
